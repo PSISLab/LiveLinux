8d2bd67f436bb885c7c1aa23d31fe07f8ae9b8fe
edn-g <edn.g@free.fr>
2013-11-27 23:03:55 +0100

    Correction de bugs et personnalisation

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ LiveLinux
 =========
 
 Tools for creating live systems, inspired from [https://help.ubuntu.com/community/LiveCDCustomizationFromScratch](https://help.ubuntu.com/community/LiveCDCustomizationFromScratch)
+
+Roadmap
+-------
+
+* _Versions futures_ 1.x
+	* Création d'un paquet .deb
+	* Vérification du périférique de destination avec la commande `write`, pour éviter un désastre
+	* Gérer le partitionnement de l'image
+	* Gérer un volume persistant privé
+	* Gérer les mises à jour du système en lecture seule
+* _Versions futures_ 2.x
+	* Restaurer une version packagée, ou importer une image ISO
+	* Gérer un volume persistant publique
+
+Changelog
+---------
+
+* _Version de développement_
+* Version __0.1__
+	* Gestion des versions
+	* Manipulation de variables avec les commandes `set` et `get`
+	* Ecriture de l'image sur un périférique avec la commande `write`
+	* Création d'une image disque ou ISO avec la command `release` 
+	* Manipulation de l'environnement avec la commande `chroot`
+	* Initialisation d'un environnement avec la command `setup`
--- llm.sh
+++ llm.sh
@@ -13,7 +13,7 @@ function usage
 	CMD="$1"
 	
 	if [ -z "$CMD" ]; then
-		echo "Usage: $0 <command> <target> [command args...]"
+		echo "Usage: $0 <target> <command> [command args...]"
 		echo ""
 		echo "	Commands :"
 		echo "		help $(usage_help)"
@@ -21,6 +21,8 @@ function usage
 		echo "		chroot $(usage_chroot)"
 		echo "		release $(usage_release)"
 		echo "		write $(usage_write)"
+		echo "		set $(usage_set)"
+		echo "		get $(usage_get)"
 	elif [ "$(type -t usage_$CMD)" == "function" ]; then
 		echo "Usage: $0 <target> $CMD $(usage_$CMD)"
 	fi
@@ -55,12 +57,12 @@ function cmd_setup
 {
 	# Check target directory
 	if [ -f "$TARGET" ] || [ -d "$TARGET" ]; then
-		echo "Target directory '$TARGET' already exists" && exit 1
+		echo foo # echo "Target directory '$TARGET' already exists" && exit 1
 	fi
 	
 	# Create target directory
 	mkdir -p "$TARGET" || exit 1
-	echo "0.0.0" > "$TARGET/version" || exit 2
+	cmd_set version "0.0.0" || exit 2
 	
 	# Bootstrap Ubuntu with same arch and release
 	mkdir -p "$CHRDIR" || return 2
@@ -71,94 +73,71 @@ function cmd_setup
 	cp "/etc/apt/sources.list" "$CHRDIR/etc/apt/sources.list" || return 2
 
 	# Install mandatory packages
-	"$0" chroot "$TARGET" DEBIAN_FRONTEND=noninteractive  apt-get install --yes ubuntu-standard casper lupin-casper discover laptop-detect os-prober linux-generic || exit 2
-	"$0" chroot "$TARGET" DEBIAN_FRONTEND=noninteractive  apt-get install --yes --no-install-recommends network-manager || exit 2
+	echo "#!/bin/bash
+DEBIAN_FRONTEND=noninteractive  apt-get install --yes ubuntu-standard casper lupin-casper discover laptop-detect os-prober linux-generic || exit 1
+DEBIAN_FRONTEND=noninteractive  apt-get install --yes --no-install-recommends network-manager || exit 1
+" > "$CHRDIR/tmp/init-chroot.sh" || return 2
+	chmod +x "$CHRDIR/tmp/init-chroot.sh" || return 2
+	cmd_chroot /tmp/init-chroot.sh || error_chroot $? || return 2
 
 	# Setup image creation
-	mkdir -p "$IMGDIR"/{casper,isolinux,install} || exit 2
+	mkdir -p "$IMGDIR"/{casper,isolinux,install} || return 2
 	
 	# Setup release storage
-	mkdir -p "$RELDIR" || exit 2
+	mkdir -p "$RELDIR" || return 2
 
 	# Setup release creation
-	mkdir -p "$FACDIR/mnt" || exit 2
-	touch "$FACDIR/loop" || exit 2
+	mkdir -p "$FACDIR/mnt" || return 2
+	touch "$FACDIR/loop" || return 2
 
 	# Generate default boot instructions
-	printf '
-************************************************************************
-
-This is an Ubuntu Remix Live CD.
-
-For the default live system, enter "live".  To run memtest86+, enter "memtest"
-
-************************************************************************
-	' > "$IMGDIR/isolinux/isolinux.txt" || return 2
+	printf "Starting $(cmd_get title)...
+" > "$IMGDIR/isolinux/isolinux.txt" || return 2
 
 	# Generate default boot config
-	printf 'DEFAULT live
-
-LABEL live
-  menu label ^Start or install Ubuntu Remix
+	printf 'DEFAULT boot
+LABEL boot
+  menu label ^Start $(cmd_get title)
   kernel /casper/vmlinuz
   append  file=/cdrom/preseed/ubuntu.seed boot=casper initrd=/casper/initrd.lz quiet splash --
 LABEL check
-  menu label ^Check CD for defects
+  menu label ^Check disk for defects
   kernel /casper/vmlinuz
   append  boot=casper integrity-check initrd=/casper/initrd.lz quiet splash --
 LABEL memtest
   menu label ^Memory test
   kernel /install/memtest
   append -
-LABEL hd
-  menu label ^Boot from first hard disk
-  localboot 0x80
-  append -
-DISPLAY isolinux.txt
-TIMEOUT 300
-PROMPT 1 
 
-#prompt flag_val
-# 
-# If flag_val is 0, display the "boot:" prompt 
-# only if the Shift or Alt key is pressed,
-# or Caps Lock or Scroll lock is set (this is the default).
-# If  flag_val is 1, always display the "boot:" prompt.
-#  http://linux.die.net/man/1/syslinux   syslinux manpage
-	' > "$IMGDIR/isolinux/isolinux.cfg" || return 2
+DISPLAY isolinux.txt
+TIMEOUT 30
+PROMPT 0 
+' > "$IMGDIR/isolinux/isolinux.cfg" || return 2
 
 	# Create diskdefines
-	printf '#define DISKNAME  Ubuntu Remix
+	printf "#define DISKNAME  $(cmd_get title)
 #define TYPE  binary
 #define TYPEbinary  1
-#define ARCH  i386
-#define ARCHi386  1
+#define ARCH  amd64
+#define ARCHamd64  1
+#define ARCHi386  0
 #define DISKNUM  1
 #define DISKNUM1  1
 #define TOTALNUM  0
 #define TOTALNUM0  1
-	' > "$IMGDIR/README.diskdefines" || return 2
+" > "$IMGDIR/README.diskdefines" || return 2
 
 	touch "$IMGDIR/ubuntu" || return 2
 	mkdir "$IMGDIR/.disk" || return 2
-	touch "$IMGDIR/.disk/base_installable" || return 2
+	#touch "$IMGDIR/.disk/base_installable" || return 2
 	echo 'full_cd/single' > "$IMGDIR/.disk/cd_type" || return 2
-	echo 'Ubuntu Remix' > "$IMGDIR/.disk/info" || return 2
-	echo 'http://www.psislab.com' > "$IMGDIR/.disk/release_notes_url" || return 2
-}
-
-function error_setup
-{
-	case $1 in
-		2)	rm -r "$TARGET"
-	esac
-	
-	return 1
+	echo "$(cmd_get title)" > "$IMGDIR/.disk/info" || return 2
+	#echo 'http://www.psislab.com' > "$IMGDIR/.disk/release_notes_url" || return 2
 }
 
 function usage_setup
 {
-	echo "[-f|--force]"
+	echo ""
 }
 
 function cmd_chroot
@@ -271,10 +250,11 @@ function cmd_release
 	local iso=no
 	local img=no
 	
-	local major="$(cat "$TARGET/version" | awk -F. '{print $1}')"
-	local minor="$(cat "$TARGET/version" | awk -F. '{print $2}')"
-	local build="$(cat "$TARGET/version" | awk -F. '{print $3}')"
-	local version=
+	local version="$(cmd_get version)" || return 1
+	local major="$(echo "$version" | awk -F. '{print $1}')" || return 1
+	local minor="$(echo "$version" | awk -F. '{print $2}')" || return 1
+	local build="$(echo "$version" | awk -F. '{print $3}')" || return 1
+	version=
 	let build++
 	
 	while [ $# -gt 0 ]; do
@@ -304,32 +284,26 @@ function cmd_release
 	if [ -z "$version" ]; then
 		version="$major.$minor.$build"
 	fi
-	echo "$version" > "$TARGET/version"
+	cmd_set version "$version" || return 1
 	
-	echo "ISO: $iso"
-	echo "IMG: $img"
-	echo "VERSION: $version"
-	
-	cp "$CHRDIR/boot/vmlinuz-"* "$IMGDIR/casper/vmlinuz" || exit 1
-	cp "$CHRDIR/boot/initrd.img-"* "$IMGDIR/casper/initrd.lz" || exit 1
-	cp "/usr/lib/syslinux/isolinux.bin" "$IMGDIR/isolinux/isolinux.bin" || exit 1
-	cp "/boot/memtest86+.bin" "$IMGDIR/install/memtest" || exit 1
+	cp "$CHRDIR/boot/vmlinuz-"* "$IMGDIR/casper/vmlinuz" || return 1
+	cp "$CHRDIR/boot/initrd.img-"* "$IMGDIR/casper/initrd.lz" || return 1
+	cp "/usr/lib/syslinux/isolinux.bin" "$IMGDIR/isolinux/isolinux.bin" || return 1
+	cp "/boot/memtest86+.bin" "$IMGDIR/install/memtest" || return 1
 
 	# Create manifest
-	chroot "$CHRDIR" dpkg-query -W --showformat='${Package} ${Version}\n' > "$IMGDIR/casper/filesystem.manifest" || exit 1
+	chroot "$CHRDIR" dpkg-query -W --showformat='${Package} ${Version}\n' > "$IMGDIR/casper/filesystem.manifest" || return 1
 
 	# Compress the chroot
-	mksquashfs "$CHRDIR" "$IMGDIR/casper/filesystem.squashfs" -noappend -e boot || exit 1
-	printf $(du -sx --block-size=1 "$CHRDIR" | cut -f1) > "$IMGDIR/casper/filesystem.size" || exit 1
+	mksquashfs "$CHRDIR" "$IMGDIR/casper/filesystem.squashfs" -noappend -e boot || return 1
+	printf $(du -sx --block-size=1 "$CHRDIR" | cut -f1) > "$IMGDIR/casper/filesystem.size" || return 1
 
 	# Calculate MD5
-	(cd "$IMGDIR" && find . -type f -print0 | xargs -0 md5sum | grep -v "\./md5sum.txt" > "md5sum.txt") || exit 1
+	(cd "$IMGDIR" && find . -type f -print0 | xargs -0 md5sum | grep -v "\./md5sum.txt" > "md5sum.txt") || return 1
 	
 	if [ "$iso" == "yes" ]; then
 		# Create ISO
-		cd "$IMGDIR" || exit 1
-		mkisofs -r -V "UbuntuRemix" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o "$RELDIR/ubuntu-remix-$version.iso" .
-		cd -
+		mkisofs -r -V "UbuntuRemix" -cache-inodes -J -l -b "$IMGDIR/isolinux/isolinux.bin" -c "$IMGDIR/isolinux/boot.cat" -no-emul-boot -boot-load-size 4 -boot-info-table -o "$RELDIR/ubuntu-remix-$version.iso" "$IMGDIR" || return 1
 	fi
 	
 	if [ "$img" == "yes" ]; then
@@ -339,38 +313,54 @@ function cmd_release
 			losetup "$loop_device" || break
 		done
 		
-		if [ -z "$loop_device" ]; then
+		if [ -n "$loop_device" ]; then
+			ERROR_RELEASE_loop_device="$loop_device"
+		else
 			return 1
 		fi
 
-		dd if=/dev/zero of="$FACDIR/diskpart.sys" bs=1 count=0 seek=250M || return 1
+		dd if=/dev/zero of="$FACDIR/diskpart.sys" bs=1 count=0 seek=350M || return 1
 		losetup "$loop_device" "$FACDIR/diskpart.sys" || return 1
-		mkfs.ext2 -L "UbuntuRemix" -m 1 "$loop_device" || return 1
+		mkfs.ext2 -L "UbuntuRemix" -m 1 "$loop_device" || return 2
 
-		mount "$loop_device" "$FACDIR/mnt" || return 1
+		mount "$loop_device" "$FACDIR/mnt" || return 2
 
-		cp -a "$IMGDIR/"* "$FACDIR/mnt/" || return 1
+		cp -a "$IMGDIR/"* "$FACDIR/mnt/" || return 3
 
-		mkdir "$FACDIR/mnt/boot" || return 1
-		mv "$FACDIR/mnt/isolinux" "$FACDIR/mnt/boot/extlinux" || return 1
-		mv "$FACDIR/mnt/boot/extlinux/isolinux.cfg" "$FACDIR/mnt/boot/extlinux/extlinux.conf" || return 1
+		mkdir "$FACDIR/mnt/boot" || return 3
+		mv "$FACDIR/mnt/isolinux" "$FACDIR/mnt/boot/extlinux" || return 3
+		mv "$FACDIR/mnt/boot/extlinux/isolinux.cfg" "$FACDIR/mnt/boot/extlinux/extlinux.conf" || return 3
 		extlinux --install "$FACDIR/mnt/boot/extlinux/" || return 1
-		umount "$loop_device" || return 1
+		umount "$loop_device" || return 2
 
-		gzip -c "$loop_device" > "$RELDIR/ubuntu-remix-$version.img.gz" || return 1
+		gzip -c "$loop_device" > "$RELDIR/ubuntu-remix-$version.img.gz" || return 2
 		losetup -d "$loop_device" || return 1
 	fi
 }
 
+function error_release
+{
+	case $1 in
+		3)	umount "$ERROR_RELEASE_loop_device"
+			losetup -d "$ERROR_RELEASE_loop_device"
+			;;
+		
+		2)	losetup -d "$ERROR_RELEASE_loop_device"
+			;;
+	esac
+	
+	return 1
+}
+
 function usage_release
 {
-	echo "[--iso] [--img [-d|--device <device>]] [[-m|--minor]|[-M|--major]|[-v|--version <major.minor.build>]"
+	echo "[--iso] [--img] [[-m|--minor]|[-M|--major]|[-v|--version <major.minor.build>]"
 }
 
 function cmd_write
 {
 	local device="$1"
-	local version="$(cat "$TARGET/version")"
+	local version="$(cmd_get version)"
 	
 	zcat "$RELDIR/ubuntu-remix-$version.img.gz" > "$device"
 }
@@ -380,24 +370,67 @@ function usage_write
 	echo "[-v|--version <version>] device"
 }
 
+function cmd_set
+{
+	local var="$1"
+	local value="$2"
+	
+	case "$var" in
+		version)
+			echo "$value" > "$TARGET/version" || return 1
+			;;
+		
+		*)	echo "Unknown var '$var'"
+			return 1
+	esac
+}
+
+function usage_set
+{
+	echo "<var> <value>"
+}
+
+function cmd_get
+{
+	local var="$1"
+	
+	case "$var" in
+		version)
+			cat "$TARGET/version" || return 1
+			;;
+		
+		title)
+			echo "LinuxLive" || return 1
+			;;
+		
+		*)	echo "Unknown var '$var'"
+			return 1
+	esac
+}
+
+function usage_get
+{
+	echo "<var>"
+}
+
 # ------------------------------------------------------------------------ #
 
 
 
 # ------------------------------------------------------------------------ #
 
-CMD="$1"; shift
 TARGET="$(readlink -m "$1")"; shift
+CMD="$1"; shift
 
 CHRDIR="$TARGET/root"
 IMGDIR="$TARGET/image"
 FACDIR="$TARGET/factory"
 RELDIR="$TARGET/releases"
 
-if [ -z "$CMD" ]; then
+if [ -z "$TARGET" ]; then
 	usage
 	exit 1
-elif [ -z "$TARGET" ]; then
+elif [ -z "$CMD" ]; then
 	usage
 	exit 1
 elif [ "$(type -t cmd_$CMD)" == "function" ]; then
